537cf63a24c7c08760cab1a8fc7982c96951ee2d
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-09-26 12:12:35 +1000

    test2

--- routes/mfa.js
+++ routes/mfa.js
@@ -21,7 +21,9 @@ router.post("/setup", async (req, res) => {
 
     const response = await client.send(command);
     const secretCode = response.SecretCode;
+    console.log("beep boop");
     console.log(secretCode);
+    console.log("boop beep");
     const issuer = "Spaghetti Sim"; // Replace with your app's name
     const otpauthURI = `otpauth://totp/${issuer}?secret=${secretCode}&issuer=${issuer}&digits=6&period=30`;
 
