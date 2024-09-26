eb39b982f564f18ed31df86c5feabfa930d07796
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-09-27 09:57:04 +1000

    added exchange route

--- routes/token.js
+++ routes/token.js
@@ -4,7 +4,8 @@ const {
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
 } = require("@aws-sdk/client-cognito-identity-provider");
-const { getClientId } = require("../utility/secretHandler");
+const { getClientId, getUserPoolId } = require("../utility/secretHandler");
+const jwt = require("aws-jwt-verify");
 
 const client = new CognitoIdentityProviderClient({ region: "ap-southeast-2" });
 
@@ -32,6 +33,43 @@ router.post("/", async (req, res) => {
   } catch (err) {
     console.error("Error exchanging authorization code for tokens:", err);
     res.status(500).json({ message: "Error exchanging code", error: err });
+  }
+});
+
+router.post("/exchange", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      error: true,
+      message: "Authorization header ('Bearer token') not found",
+    });
+  }
+
+  try {
+    const bearerToken = req.headers.authorization.split(" ");
+
+    if (bearerToken.length !== 2 || bearerToken[0] !== "Bearer") {
+      return res.status(401).json({ error: true, message: "Authorization header is malformed" });
+    }
+    const token = bearerToken[1];
+    console.log("Token: ", token);
+    const client_id = await getClientId();
+    const pool_id = await getUserPoolId();
+
+    const idVerifier = jwt.CognitoJwtVerifier.create({
+      userPoolId: pool_id,
+      tokenUse: "id",
+      clientId: client_id,
+    });
+
+    const IdTokenVerifyResult = await idVerifier.verify(token);
+
+    return res.status(200).json({ email: IdTokenVerifyResult.email });
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: true, message: "JWT token has expired" });
+    } else {
+      return res.status(401).json({ error: true, message: "Invalid JWT token" });
+    }
   }
 });
 
