dd68723b15478755136be62e93d89e0e0d7b2d8f
Scott May <129928587+Scotto808@users.noreply.github.com>
2024-09-27 09:59:58 +1000

    fixed

--- routes/token.js
+++ routes/token.js
@@ -9,34 +9,7 @@ const jwt = require("aws-jwt-verify");
 
 const client = new CognitoIdentityProviderClient({ region: "ap-southeast-2" });
 
-router.post("/", async (req, res) => {
-  const { code } = req.body;
-
-  try {
-    const clientID = await getClientId();
-    const command = new InitiateAuthCommand({
-      AuthFlow: "AUTHORIZATION_CODE",
-      ClientId: clientID, // Your Cognito App Client ID
-      AuthParameters: {
-        CODE: code,
-        REDIRECT_URI: "http://localhost:3000/login", // Your redirect URI
-      },
-    });
-
-    const response = await client.send(command);
-    console.log("Exchanging code for tokens:", response);
-    res.json({
-      idToken: response.AuthenticationResult.IdToken,
-      accessToken: response.AuthenticationResult.AccessToken,
-      refreshToken: response.AuthenticationResult.RefreshToken,
-    });
-  } catch (err) {
-    console.error("Error exchanging authorization code for tokens:", err);
-    res.status(500).json({ message: "Error exchanging code", error: err });
-  }
-});
-
-router.post("/exchange", async (req, res) => {
+router.post("/token", async (req, res) => {
   if (!req.headers.authorization) {
     return res.status(401).json({
       error: true,
